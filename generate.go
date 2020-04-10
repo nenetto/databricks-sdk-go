10efaa5a792b85229e97026364659991b35968bc
Eugenio Marinetto <nenetto@gmail.com>
2020-04-10 14:18:06 +0200

    Auto stash before checking out "HEAD"

--- generate.go
+++ generate.go
@@ -3,5 +3,3 @@ package sdk
 //go:generate java -jar swagger/swagger-codegen-cli-2.2.1.jar generate --config swagger/config.json -Dmodels -l go -i swagger/databricks-2.0.yaml -o models
 //go:generate rm models/api_client.go models/api_response.go models/configuration.go models/.gitignore models/README.md
 //go:generate gofmt -s -w models
-
-
